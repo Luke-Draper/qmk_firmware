24c681f54fab91a237bca36f2d943296c931819a
DerpLlama <ldraper@watford-city.k12.nd.us>
2020-10-20 16:35:31 -0500

    Functional

--- keyboards/xd75/keymaps/tiger_board/keymap.c
+++ keyboards/xd75/keymaps/tiger_board/keymap.c
@@ -31,16 +31,19 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY (Designed for the computer to switch to DVORAK)
+ * 
+ * Notice that left control and shift are not mirrored with right control and shift
+ * 
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 1 -> 1 | 2 -> 2 | 3 -> 3 | 4 -> 4 | 5 -> 5 | - -> [ | \ -> \ | = -> ] | 6 -> 6 | 7 -> 7 | 8 -> 8 | 9 -> 9 | 0 -> 0 | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * |        | Q -> ' | W -> , | E -> . | R -> p | T -> y | ' -> - | [ -> / | ] -> = | Y -> f | U -> g | I -> c | O -> r | P -> l |        |
+ * | LCTRLa | Q -> ' | W -> , | E -> . | R -> p | T -> y | ' -> - | [ -> / | ] -> = | Y -> f | U -> g | I -> c | O -> r | P -> l | RCTRLv |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | TAB    | A -> a | S -> o | D -> e | F -> u | G -> i | ` -> ` | UP     | DELETE | H -> d | J -> h | K -> t | L -> n | ; -> s | ENTER  |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z -> ; | X -> q | C -> j | V -> k | B -> x | LUFT   | DOWN   | RIGHT  | N -> b | M -> m | , -> w | . -> v | / -> z | RCTRL  |
+ * | LSHIFT | Z -> ; | X -> q | C -> j | V -> k | B -> x | LEFT   | DOWN   | RIGHT  | N -> b | M -> m | , -> w | . -> v | / -> z | RCTRL  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * |        |        | LGUI   | LALT   | LCTRL  | SPACE  |        |        |        | SPACE  | RSHIFT | RALT   | RGUI   |        |        |
+ * | LCTRLz | LCTRLy | LGUI   | LALT   | LCTRL  | SPACE  | TO QWE | TO FUN | TO GAM | SPACE  | RSHIFT | RALT   | RGUI   | RCTRLx | RCTRLc |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -49,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_QUOT,   KC_LBRC,   KC_RBRC,   KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      ,
     KC_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_GRV,    KC_DEL,    KC_PGUP,   KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_ENT,
     KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_END,    KC_UP,     KC_PGDN,   KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
-    KC_LCTL,   KC_LGUI,   KC_LALT,   MO(_FN),   KC_SPC,    KC_SPC,    KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_SPC,    KC_SPC,    MO(_FN),   KC_RALT,   KC_RGUI,   KC_RCTL
+    KC_LCTL,   KC_LGUI,   KC_LALT,   MO(_FUNCTION),   KC_SPC,    KC_SPC,    KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_SPC,    KC_SPC,    MO(_FUNCTION),   KC_RALT,   KC_RGUI,   KC_RCTL
   ),
 
 /* FUNCTION
@@ -66,12 +69,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
-  [_FN] = LAYOUT_ortho_5x15( /* FUNCTION */
+  [_FUNCTION] = LAYOUT_ortho_5x15( /* FUNCTION */
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS,
     KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, RESET,   _______, _______, _______,
     KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______,
-    _______, _______, RGB_TOG, MO(_FN), RGB_RMOD,RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, MO(_FN), _______, _______, _______
+    _______, _______, RGB_TOG, MO(_FUNCTION), RGB_RMOD,RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, MO(_FUNCTION), _______, _______, _______
   )
 };
 
@@ -88,5 +91,5 @@ void matrix_scan_user(void) {
 }
 
 void led_set_user(uint8_t usb_led) {
-
+  keycaps_led_on();
 }
